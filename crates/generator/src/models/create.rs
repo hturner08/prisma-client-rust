76d0fb04ea065556330a5c41ffdbcd405b5bfa79
hturner08 <herbie@trykino.com>
2024-03-23 11:09:03 -0400

    Init

---
+++ crates/generator/src/models/create.rs
@@ -0,0 +1,131 @@
+use prisma_client_rust_sdk::prisma::{
+    prisma_models::walkers::ModelWalker, psl::parser_database::ScalarFieldType,
+};
+
+use crate::prelude::*;
+
+use super::required_fields;
+
+fn create_unchecked(model: ModelWalker) -> Option<TokenStream> {
+    required_fields(model)?;
+
+    let model_name_snake = snake_ident(model.name());
+
+    let (names, types): (Vec<_>, Vec<_>) = model
+        .scalar_fields()
+        .filter_map(|field| {
+            let name_snake = snake_ident(field.name());
+
+            if !field.required_on_create() {
+                return None;
+            }
+
+            Some((
+                name_snake,
+                match field.scalar_field_type() {
+                    ScalarFieldType::CompositeType(id) => {
+                        let comp_type = model.db.walk(id);
+
+                        let comp_type_snake = snake_ident(comp_type.name());
+
+                        quote!(super::#comp_type_snake::Create)
+                    }
+                    _ => field.type_tokens(&quote!(super::))?,
+                },
+            ))
+        })
+        .unzip();
+
+    Some(quote! {
+        #[derive(Debug, Clone)]
+        pub struct CreateUnchecked {
+            #(pub #names: #types,)*
+            pub _params: Vec<UncheckedSetParam>
+        }
+
+        impl CreateUnchecked {
+             pub fn to_query<'a>(self, client: &'a PrismaClient) -> CreateUncheckedQuery<'a> {
+                client.#model_name_snake()
+                    .create_unchecked(
+                        #(self.#names,)*
+                        self._params
+                    )
+            }
+
+            pub fn to_params(mut self) -> Vec<UncheckedSetParam> {
+                self._params.extend([
+                    #(#names::set(self.#names)),*
+                ]);
+
+                self._params
+            }
+        }
+
+        pub fn create_unchecked(#(#names: #types,)* _params: Vec<UncheckedSetParam>)
+            -> CreateUnchecked {
+            CreateUnchecked {
+                #(#names,)*
+                _params
+            }
+        }
+    })
+}
+
+fn create(model: ModelWalker) -> Option<TokenStream> {
+    let model_name_snake = snake_ident(model.name());
+
+    let (names, (types, push_wrappers)): (Vec<_>, (Vec<_>, Vec<_>)) = required_fields(model)?
+        .into_iter()
+        .map(|field| {
+            (
+                snake_ident(field.inner.name()),
+                (field.typ, field.push_wrapper),
+            )
+        })
+        .unzip();
+
+    Some(quote! {
+       #[derive(Debug, Clone)]
+        pub struct Create {
+            #(pub #names: #types,)*
+            pub _params: Vec<SetParam>
+        }
+
+        impl Create {
+            pub fn to_query<'a>(self, client: &'a PrismaClient) -> CreateQuery<'a> {
+                client.#model_name_snake()
+                    .create(
+                        #(self.#names,)*
+                        self._params
+                    )
+            }
+
+            pub fn to_params(mut self) -> Vec<SetParam> {
+                self._params.extend([
+                    #(#names::#push_wrappers(self.#names)),*
+                ]);
+
+                self._params
+            }
+        }
+
+        pub fn create(#(#names: #types,)* _params: Vec<SetParam>)
+            -> Create {
+            Create {
+                #(#names,)*
+                _params
+            }
+        }
+    })
+}
+
+pub fn types(model: ModelWalker) -> TokenStream {
+    let create_unchecked = create_unchecked(model);
+    let create = create(model);
+
+    quote! {
+        #create
+
+        #create_unchecked
+    }
+}
---
+++ crates/sdk/src/lib.rs
@@ -0,0 +1,144 @@
+mod args;
+mod casing;
+pub mod dmmf;
+mod extensions;
+mod jsonrpc;
+mod keywords;
+mod runtime;
+mod shared_config;
+mod utils;
+
+use proc_macro2::TokenStream;
+use serde::{de::DeserializeOwned, Serialize};
+use serde_json::{Map, Value};
+use thiserror::Error;
+
+use runtime::GeneratorMetadata;
+
+pub use args::GenerateArgs;
+pub use casing::*;
+pub use extensions::*;
+pub use quote::quote;
+
+use crate::prelude::snake_ident;
+
+pub mod prisma {
+    pub use dmmf;
+    pub use prisma_models;
+    pub use psl;
+    pub use query_core;
+    pub use request_handlers;
+}
+
+pub mod prelude {
+    pub use super::{args::*, prisma::*, *};
+    pub use proc_macro2::*;
+    pub use quote::*;
+    pub use syn::Ident;
+
+    pub fn ident(name: &str) -> Ident {
+        format_ident!("{name}")
+    }
+
+    pub fn snake_ident(name: &str) -> Ident {
+        format_ident!("{}", name.to_case(Case::Snake, false))
+    }
+
+    pub fn snake_ident_raw(name: &str) -> Ident {
+        format_ident!("{}", name.to_case(Case::Snake, true))
+    }
+
+    pub fn pascal_ident(name: &str) -> Ident {
+        format_ident!("{}", name.to_case(Case::Pascal, false))
+    }
+}
+
+pub type GenerateFn = fn(GenerateArgs, Map<String, Value>) -> GenerateResult;
+pub type GenerateResult = Result<Module, GeneratorError>;
+
+#[derive(Debug, Error)]
+pub enum GeneratorError {
+    #[error("Schema contains invalid names \n{0}")]
+    ReservedNames(String),
+    #[error("Failed to create client file: {0}")]
+    FileCreate(std::io::Error),
+    #[error("Failed to write generated client to file: {0}")]
+    FileWrite(std::io::Error),
+    #[error("Failed to deserialize generator arguments: {0}")]
+    ArgDeserialize(serde_json::Error),
+    #[error("Generator {name} failed: \n{message}")]
+    InternalError { name: &'static str, message: String },
+}
+
+pub struct Module {
+    pub name: String,
+    pub contents: TokenStream,
+    pub submodules: Vec<Module>,
+}
+
+impl Module {
+    pub fn new(name: &str, contents: TokenStream) -> Self {
+        Self {
+            name: name.to_string(),
+            contents,
+            submodules: vec![],
+        }
+    }
+
+    pub fn add_submodule(&mut self, submodule: Module) {
+        self.submodules.push(submodule);
+    }
+
+    pub fn flatten(self) -> TokenStream {
+        let contents = self.contents;
+
+        let submodule_contents = self
+            .submodules
+            .into_iter()
+            .map(|sm| {
+                let name = snake_ident(&sm.name);
+                let contents = sm.flatten();
+
+                quote! {
+                    pub mod #name {
+                        #contents
+                    }
+                }
+            })
+            .collect::<Vec<_>>();
+
+        quote! {
+            #contents
+
+            #(#submodule_contents)*
+        }
+    }
+}
+
+pub trait PrismaGenerator: DeserializeOwned {
+    const NAME: &'static str;
+    const DEFAULT_OUTPUT: &'static str;
+
+    type Error: Serialize + std::error::Error;
+
+    fn generate(self, args: GenerateArgs) -> Result<Module, Self::Error>;
+
+    fn erased_generate(args: GenerateArgs, config: Map<String, Value>) -> GenerateResult
+    where
+        Self: Sized,
+    {
+        let generator = serde_json::from_value::<Self>(Value::Object(config))
+            .map_err(GeneratorError::ArgDeserialize)?;
+
+        generator
+            .generate(args)
+            .map_err(|e| GeneratorError::InternalError {
+                name: Self::NAME,
+                message: e.to_string(),
+            })
+    }
+
+    fn run() {
+        GeneratorMetadata::new(Self::erased_generate, Self::NAME, Self::DEFAULT_OUTPUT).run();
+    }
+}
